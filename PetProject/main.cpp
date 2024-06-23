efed04aeb937e2bf1c9c75e6c18cc365d4d5c6b3
- - <kulchitskiy.666@gmail.com>
2024-06-24 00:32:33 +0300

    Refactor: replace class Conditions with struct

--- PetProject/main.cpp
+++ PetProject/main.cpp
@@ -22,7 +22,6 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
-unsigned int loadCubemap(std::vector<std::string> faces);
 
 const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 1200;
@@ -36,9 +35,8 @@ bool firstMouse = true;
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-class Conditions
+struct Conditions
 {
-public:
     bool drawModel = false;
     bool loadModel = false;
     bool isMRpressed = false;
